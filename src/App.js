23ac5f182dda4d9bf926403f342ad6bdde6a2e5a
Musyimi97 <collinsmusyimi.cm@gmail.com>
2020-02-24 14:09:25 +0300

    Todos component created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import Todos from './components/Todos'; 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <h1>App</h1>
+      <Todos/>
       </header>
     </div>
   );
